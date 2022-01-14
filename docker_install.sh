36bc6a34c29c1e64066eec3ff167dfb15cbc1f5d
Patrick McGuire <insidenothing@gmail.com>
2022-01-14 10:14:22 -0500

    Update and rename apt_install.sh to docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,7 +1,12 @@
+# apt
 sudo apt install -y curl
+# yum
+sudo yum install -y curl
+# Common
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+sudo docker build .
 whattorun=$(sudo docker images -q | head -n 1)
 sudo docker run -i -d -p 19126:19126 $whattorun
-whattodebug=$(docker ps | awk -v f=1 '$f ~ /^[[:alnum:]]{12}$/ { print $f }')
-docker exec -ti $whattodebug bash
+whattodebug=$(sudo docker ps | awk -v f=1 '$f ~ /^[[:alnum:]]{12}$/ { print $f }')
+sudo docker exec -ti $whattodebug bash
