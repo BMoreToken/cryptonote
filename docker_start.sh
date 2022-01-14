4a8be70e3ce530b46928cb9625ba1968c045b86f
Patrick McGuire <insidenothing@gmail.com>
2022-01-14 11:42:43 -0500

    Update docker_start.sh

--- docker_start.sh
+++ docker_start.sh
@@ -1,4 +1,3 @@
 whattorun=$(sudo docker images -q | head -n 1)
 sudo docker run -i -d -p 19126:19126 $whattorun
-whattodebug=$(sudo docker ps | awk -v f=1 '$f ~ /^[[:alnum:]]{12}$/ { print $f }')
-sudo docker exec -ti $whattodebug bash
+echo " run docker_bash.sh to enter the container "
