d032b5c2532fb0f2f2d7eca16ae8ec5c29e55bc6
Patrick McGuire <insidenothing@gmail.com>
2022-01-14 10:24:29 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 chmod -x docker_install.sh
+chmod -x docker_startup.sh
 chmod -x docker_bash.sh
 chmod -x update.sh
 git pull
 chmod +x docker_install.sh
+chmod +x docker_startup.sh
 chmod +x docker_bash.sh
 chmod +x update.sh
