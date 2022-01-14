7063f51aae21d1802bb9432bdae123b105e4819e
Patrick McGuire <insidenothing@gmail.com>
2022-01-14 12:38:24 -0500

    fix path

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ chmod +x docker_bash.sh
 chmod +x update.sh
 echo "Installing startup and shutdown scripts"
 sudo cp /home/ubuntu/bmorecoin/docs/start_bmorecoind.service /etc/systemd/system/start_bmorecoind.service
-sudo cp /home/ubuntu/bmorecoin/docs/stop_bmorecoind.service /etc/systemd/system stop_bmorecoind.service
+sudo cp /home/ubuntu/bmorecoin/docs/stop_bmorecoind.service /etc/systemd/system/stop_bmorecoind.service
 sudo systemctl daemon-reload
 sudo systemctl enable start_bmorecoind.service
 sudo systemctl enable stop_bmorecoind.service
