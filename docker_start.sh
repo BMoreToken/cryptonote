74b02bee95ba9ae390ec02eecde6387b87e41ead
Patrick McGuire <insidenothing@gmail.com>
2022-01-14 11:44:37 -0500

    Update docker_start.sh

--- docker_start.sh
+++ docker_start.sh
@@ -1,3 +1,3 @@
 whattorun=$(sudo docker images -q | head -n 1)
 sudo docker run -i -d -p 19126:19126 $whattorun
-echo " run docker_bash.sh to enter the container "
+echo "run ./docker_bash.sh to enter the container "
