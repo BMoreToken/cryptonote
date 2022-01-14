ce12171d8b4c05d18ca65daeeefda62501ce9008
Patrick McGuire <insidenothing@gmail.com>
2022-01-14 12:26:48 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-x=30
-while [ $x -gt 0 ]
-do
-sleep 1s
-clear
-echo "$x seconds until building a new node and wallet, press crtl-c to stop and save current wallet"
-x=$(( $x - 1 ))
-done
 ./docker_stop.sh
 chmod -x docker_install.sh
 chmod -x docker_start.sh
@@ -21,5 +12,14 @@ chmod +x docker_stop.sh
 chmod +x docker_bash.sh
 chmod +x install.sh
 chmod +x update.sh
+#!/bin/bash
+x=30
+while [ $x -gt 0 ]
+do
+sleep 1s
+clear
+echo "$x seconds until building a new node and wallet, press crtl-c to stop to save current wallet then use ./docker_start.sh to restart current wallet."
+x=$(( $x - 1 ))
+done
 docker build .
 ./docker_start.sh
