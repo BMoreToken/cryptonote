8ef521ceec1859a4fc424a1e33cb7f54d2d103c8
Patrick McGuire <insidenothing@gmail.com>
2022-01-14 12:29:29 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
+echo "Updating Code"
 git pull
+echo "Setting Permissions"
 chmod +x docker_install.sh
 chmod +x docker_start.sh
 chmod +x docker_stop.sh
 chmod +x docker_bash.sh
 chmod +x update.sh
+echo "Installing startup and shutdown scripts"
 sudo cp docs/bmorecoind.service /etc/systemd/system/bmorecoind.service
 sudo cp docs/shutdown_node.service /etc/systemd/system shutdown_node.service
 sudo systemctl daemon-reload
 sudo systemctl enable start_bmorecoind.service
 sudo systemctl enable stop_bmorecoind.service
+echo "Installing Docker"
 ./docker_install.sh
