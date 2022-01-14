7d214a0ce9cc80883af1fdc801dbc6fe107d6f87
Patrick McGuire <insidenothing@gmail.com>
2022-01-14 10:14:58 -0500

    Update docker_bash.sh

--- docker_bash.sh
+++ docker_bash.sh
@@ -1,2 +1,2 @@
-whattodebug=$(docker ps | awk -v f=1 '$f ~ /^[[:alnum:]]{12}$/ { print $f }')
-docker exec -ti $whattodebug bash
+whattodebug=$(sudo docker ps | awk -v f=1 '$f ~ /^[[:alnum:]]{12}$/ { print $f }')
+sudo docker exec -ti $whattodebug bash
